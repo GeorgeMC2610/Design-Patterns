d17cca6992891c276c61e869089569cfb29e794e
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2022-07-06 00:56:34 +0300

    made the shape and square classes in liskov

---
+++ Exercise 3/Liskov/Shape.py
@@ -0,0 +1,10 @@
+class Shape:
+
+    def area(self):
+        pass
+
+    def set_width(self, length):
+        pass
+
+    def set_height(self, length):
+        pass
---
+++ Exercise 3/Liskov/Square.py
@@ -0,0 +1,23 @@
+from Shape import Shape
+
+class Square(Shape):
+
+    def __init__(self, length) -> None:
+
+        self.width = length
+        self.height = length
+
+    def set_width(self, length):
+
+        self.width = length
+        self.height = length
+
+    def set_height(self, length):
+
+        self.width = length
+        self.height = length
+
+
+    def area(self):
+
+        return self.width * self.height
