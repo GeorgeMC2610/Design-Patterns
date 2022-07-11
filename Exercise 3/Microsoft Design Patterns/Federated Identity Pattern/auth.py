7412e3da15d2e98fe1ba8401823c258814acee9e
Giorgos Seimenis <giorgosseimenis@gmail.com>
2022-07-11 04:49:52 +0300

    i made this work somehow

--- Exercise 3/Microsoft Design Patterns/Federated Identity Pattern/auth.py
+++ Exercise 3/Microsoft Design Patterns/Federated Identity Pattern/auth.py
@@ -10,18 +10,22 @@ class Auth:
         self.application_b = ApplicationB()
         self.application_c = ApplicationC()
 
-    def login(self, username, password):
+    def login(self, username, password) -> bool:
         if username not in self.registered_accounts.keys():
             print("Wrong credentials.") 
+            return False
         
         if self.registered_accounts[username] != password:
             print("Wrong credentials.")
+            return False
         
         print(f"Welcome back, {username}!")
+        return True
     
-    def register(self, username, password):
+    def register(self, username, password) -> bool:
         if username in self.registered_accounts.keys():
             print("Username already exists.")
+            return False
         
         print("Encrypting passwords...")
         self.registered_accounts.update( {username : password} )
@@ -31,6 +35,7 @@ class Auth:
         self.application_c.register(username, password)
 
         print(f"Welcome! Nice to have you, {username}. You have now access to all applications!")
+        return True
 
     def useful_function_a(self, username, password):
 
@@ -39,10 +44,10 @@ class Auth:
 
     def useful_function_b(self, username, password):
 
-        self.application_a.login(username, password)
-        self.application_a.useful_function()
+        self.application_b.login(username, password)
+        self.application_b.useful_function()
 
     def useful_function_c(self, username, password):
 
-        self.application_a.login(username, password)
-        self.application_a.useful_function()
+        self.application_c.login(username, password)
+        self.application_c.useful_function()
--- Exercise 3/Microsoft Design Patterns/Federated Identity Pattern/main.py
+++ Exercise 3/Microsoft Design Patterns/Federated Identity Pattern/main.py
@@ -2,3 +2,55 @@
 # we want to have a central authentication and authorization service to access several applications
 # so create several classes to do so.
 
+import sys
+from auth import Auth
+
+auth1 = Auth()
+
+
+print("=== FEDERATED IDENTITY PATTERN ===\n\n")
+
+print("AVAILABLE ACTIONS:")
+print("Press '0' to exit.")
+print("Press '1' to login.")
+print("Press '2' to register.\n")
+
+
+available_actions = {
+    '1' : auth1.login,
+    '2' : auth1.register
+}
+
+while True:
+    action = input("Select an action --> ")
+
+    if action == '0':
+        sys.exit()
+
+    while action not in available_actions:
+        action = input("Wrong action, please enter one from the available ones --> ")
+
+    input_username = input("Provide your username --> ")
+    input_password = input("Provide your password --> ")
+
+    if available_actions[action](input_username, input_password) and action == '1':
+        pass
+    else:
+        continue
+
+    while True:
+
+        other_available_actions = {
+            'a' : auth1.useful_function_a,
+            'b' : auth1.useful_function_b,
+            'c' : auth1.useful_function_c
+        }
+
+        other_action = input(f"{input_username}, please select an application (a, b or c) --> ")
+
+        while other_action not in other_available_actions:
+            other_action = input("Wrong application, please enter one from the available ones --> ")
+        
+        other_available_actions[other_action](input_username, input_password)
+        sys.exit()
+
